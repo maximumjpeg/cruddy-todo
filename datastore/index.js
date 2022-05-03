02a52a0d3aeea7ba9d277e458b690b4c3ad34101
nelliott82 <nikkoelliott@gmail.com>
2022-05-03 14:45:50 -0400

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -63,14 +63,15 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  exports.readOne(id, function(err) {
+    if (err) {
+      callback(err);
+    } else {
+      fs.unlink(path.join(exports.dataDir, id + '.txt'), function() {
+        callback(null, '');
+      });
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
